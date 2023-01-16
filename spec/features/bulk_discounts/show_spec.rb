65b358ec1741633d7b68a72dc9954afb01250a72
Brandon Gray <brandonjoe88@hotmail.com>
2023-01-16 09:52:34 -0700

    test: merchant discount edit

--- spec/features/bulk_discounts/show_spec.rb
+++ spec/features/bulk_discounts/show_spec.rb
@@ -21,6 +21,38 @@ RSpec.describe 'bulk discount show' do
         expect(page).to have_content("Discount: #{@bulk_discount_1.discount}%")
         expect(page).to have_content("Threshold: #{@bulk_discount_1.threshold} Items")
       end
+
+      describe 'shows a link to edit the bulk discount which when clicked a new page opens' do 
+        describe 'a form to edit the discount with discounts current attributes are pre-poluated in the form' do
+          describe 'when any/all of the information is changed and click submit redirects to the bulk discount\'s show page' do
+            it 'shows that the discount\'s attributes have been updated' do
+              click_on("Edit")
+
+              expect(current_path).to eq(edit_merchant_bulk_discount_path(@merchant1, @bulk_discount_1))
+
+              expect(page).to have_field('discount', with: @bulk_discount_1.discount)
+              expect(page).to have_field('threshold', with: @bulk_discount_1.threshold)
+
+              fill_in('discount', with: 75)
+
+              click_on('Update Bulk Discount')
+
+              expect(current_path).to eq(merchant_bulk_discount_path(@merchant1, @bulk_discount_1))
+              expect(page).to have_field('discount', with: 75)
+
+              click_on("Edit")
+              
+              fill_in('discount', with: 85)
+              fill_in('threshold', with: 100)
+
+              click_on('Update Bulk Discount')
+
+              expect(page).to have_field('discount', with: 85)
+              expect(page).to have_field('threshold', with: 100)
+            end
+          end
+        end
+      end
     end
   end
 end
